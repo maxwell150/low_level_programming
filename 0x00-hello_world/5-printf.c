776def2002944065b549f4ec367000ed5d7bd12e
maxwell150 <maxwelgithumbi@gmail.com>
2021-06-17 18:36:18 +0000

    print string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - print string
+*
+*Description:  prints  with proper grammar, but the outcome is a piece of art,
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\,");
+	return (0);
+
+}
