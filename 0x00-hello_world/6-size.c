4c9bcde58f82ee861ea69be5bb4ca05f4c14d7ac
maxwell150 <maxwelgithumbi@gmail.com>
2021-06-17 19:11:32 +0000

    size of vars

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+/**
+* main - prints size
+*Description: prints size of the various variables in c in bytes
+*Return: 0
+*/
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	print("Size of a long int: %d  byte(s)", sizeof(long int));
+	printf("Size of a float: %d byte(s)", sizeof(float));
+
+	return (0);
+}
