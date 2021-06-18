89ecfc2faea2af9dc9aeb3fbce4d19174d542a2a
maxwell150 <maxwelgithumbi@gmail.com>
2021-06-18 18:36:55 +0000

    rev alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - prints alphabet
+* Description: alphabet in reverse
+* Return: 0
+*/
+int main(void)
+{
+	char x = 'z';
+
+	while (x >= 'a')
+	{
+		putchar(x);
+		x--;
+	}
+
+	putchar('\n');
+	return (0);
+}
