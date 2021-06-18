c7dec33cfc7d5c9be1f459ca63693444d4a428f5
maxwell150 <maxwelgithumbi@gmail.com>
2021-06-18 10:01:51 +0000

    positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - prints interger n
+* Description: prints whether n is +ve or not
+*
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else
+		printf"%d is negative\n", n);
+	return (0);
+}
