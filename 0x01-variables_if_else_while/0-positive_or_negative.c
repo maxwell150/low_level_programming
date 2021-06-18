5af7b983de439cb740a2225b2e50a514f1ccd693
maxwell150 <maxwelgithumbi@gmail.com>
2021-06-18 10:11:56 +0000

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,11 +13,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n == 0)
+	{
 		printf("%d is zero", n);
+	}
 	if (n > 0)
+	}
 		printf("%d is positive\n", n);
+	}
 	else
+	{
 		printf"%d is negative\n", n);
+	}
 	return (0);
 }
