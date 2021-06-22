7057a1bfe04d433cb16c4ddbb22850e8421ee5e0
maxwell150 <maxwelgithumbi@gmail.com>
2021-06-22 09:24:03 +0000

    functions

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include <holberton.h>
+/**
+* main - prints a string
+* Description: the string is "Holberton"
+*
+* Return: 0
+*/
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n\n');
+
+	return (0);
+}
