2e2f1f183e8f4449ef211b37b51d6d035c8cbfa0
maxwell150 <maxwelgithumbi@gmail.com>
2021-06-22 10:13:52 +0000

    check lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* _islower - checks whether a character is lower or upper case
+* @c: is an parameter for an argument
+* Return: 0
+*
+*/
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
