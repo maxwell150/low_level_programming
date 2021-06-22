4717536f27b20e0287441d5171940ee0d64744a7
maxwell150 <maxwelgithumbi@gmail.com>
2021-06-22 09:41:22 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <holberton.h>
+/**
+* print_alphabet - prints the alphabet
+*
+*/
+void print_alphabet(void)
+{
+	int x;
+
+	for (x =  97; x <= 122; x++)
+		_putchar(x);
+
+	_putchar('\n');
+	return (0);
+}
