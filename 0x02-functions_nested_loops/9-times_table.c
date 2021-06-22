fcc7100b45c7332a68b803392be9515d3cd4403b
maxwell150 <maxwelgithumbi@gmail.com>
2021-06-22 20:55:24 +0000

    table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * times_table - prints times table
+ */
+void times_table(void)
+{
+	int i, j, prod;
+	int x, y, z;
+
+	int x = 0;
+
+	while (x < 10)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (y == 0)
+			{
+				_putchar(48);
+				continue;
+			}
+			z = x * y;
+			_putchar(',');
+			_putchar(' ');
+			if (z >= 10)
+			{
+				_putchar(z / 10 + 48);
+				_putchar(z % 10 + 48);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(z + 48);
+			}
+		x++;
+		}
+	_putchar('\n');
+	}
+}
