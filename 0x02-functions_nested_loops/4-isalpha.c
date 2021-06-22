bf928d48ddd7bac28d3eb82e7b90582764fe6612
maxwell150 <maxwelgithumbi@gmail.com>
2021-06-22 10:28:53 +0000

    check upper or lower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "holberton.c"
+/**
+* _isalpha - check for upper of lower case
+* @c: parameter
+* Return: 0
+*
+*/
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+		return (1);
+
+	return (0);
+}
