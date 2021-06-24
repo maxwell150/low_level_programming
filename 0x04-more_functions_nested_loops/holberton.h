d914b240d69f23e58203bb565efb47d62b8de7f5
maxwell150 <maxwelgithumbi@gmail.com>
2021-06-24 20:08:16 +0000

    header

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,39 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char);
+
+/* 0-isupper.c */
+int _isupper(int);
+
+/* 1-isdigit.c */
+int _isdigit(int);
+
+/* 2-mul.c */
+int mul(int a, int b);
+
+/* 3-print_numbers.c */
+void print_numbers(void);
+
+/* 4-print_most_numbers.c */
+void print_most_numbers(void);
+
+/* 5-more_numbers.c */
+void more_numbers(void);
+
+/* 6-print_line.c */
+void print_line(int n);
+
+/* 7-print_diagonal.c */
+void print_diagonal(int);
+
+/* 8-print_square.c */
+void print_square(int size);
+
+/* 10-print-triangle.c */
+void print_triangle(int size);
+
+/* 101-print_number.c */
+void print_number(int n);
+
+#endif
