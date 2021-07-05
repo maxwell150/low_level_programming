0b7ea41dcd3afb6adb6b9881eb60e51bf3f495a7
maxwell150 <maxwelgithumbi@gmail.com>
2021-07-05 20:14:03 +0000

    update

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,22 +1,20 @@
 #include "holberton.h"
+
 /**
-* _memcpy - copies memory
-* @dest: destination string
-* @src: source str
-* @n: n.o of bytes to be copied
-* Return: pointer to dest
-*/
+ * _memcpy - copies memory
+ * @dest: destination str
+ * @src: source str
+ * @n: number of bytes to copy
+ * Return: pointer to dest
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-        char *ptr;
-        *ptr = dest;
+	char *ptr = dest;
 
-        while (n--)
-                *dest++ = *src++;
-        return (ptr);
+	while (n--)
+		*dest++ = *src++;
+	return (ptr);
 }
-
-
 
 
 
