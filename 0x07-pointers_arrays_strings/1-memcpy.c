08f5e5f433335b5bce79506f19610e70ebd997b4
maxwell150 <maxwelgithumbi@gmail.com>
2021-07-05 20:06:11 +0000

    update

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,10 +8,25 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *ptr;
-	*ptr = dest;
+        char *ptr;
+        *ptr = dest;
 
-	while (n--)
-		*dest++ = *src++;
-	return (ptr);
+        while (n--)
+                *dest++ = *src++;
+        return (ptr);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
