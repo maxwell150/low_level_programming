4bed789bf3b0dd031bd88f96b70125112de59bff
maxwell150 <maxwelgithumbi@gmail.com>
2021-07-15 21:10:52 +0000

    bug fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,17 +1,32 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include "holberton.h"
 /**
- *string_nconcat -concatenates two strings upto n
- *
- *@s1:string1
- *@s2:string2
- *@n:length of string2 to concatenate
- *Return:pointer
+ * _strlen - counts the length of string.
+ * @s: Value string check.
+ * Return: String.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0';)
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * string_nconcat - concatenate two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: bytes for s2 to use.
+ * Return: Only both strings.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, len1, len2, length, j;
-	char *p;
+
+	unsigned int i, len1, len2;
+	char *c;
 
 	if (s1 == NULL)
 	{
@@ -21,30 +36,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-	len1 = 0;
-	for (i = 0; *(s1 + i) != '\0'; i++)
-		len1++;
-	len2 = 0;
-	for (i = 0; *(s2 + i) != '\0'; i++)
-		len2++;
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
 	if (n >= len2)
 	{
 		n = len2;
 	}
-	length = len1 + n;
-	p = malloc((sizeof(char) * length) + 1);
-	if (p == NULL)
-	{
+
+	c = malloc(sizeof(char) * (len1 + n + 1));
+	if (c == NULL)
 		return (NULL);
-	}
+
 	for (i = 0; i < len1; i++)
-	{
-		p[i] = s1[i];
-	}
-	for (j = 0; i < length; j++, i++)
-	{
-		p[i] = s2[j];
-	}
-	p[i] = '\0';
-	return (p);
+		c[i] = s1[i];
+	for (; i < len1 + n; i++)
+		c[i] = s2[i - len1];
+	c[len1 + n] = '\0';
+
+	return (c);
 }
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,29 +1,28 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include "holberton.h"
+
 /**
- *_calloc - allocates memory for an array;
- *@nmemb:no of elements
- *@size:size of elements
- *Return:pointer
+ *_calloc -allocates memory for an array
+ *@nmemb: number of element in the array
+ *@size: bytes for each position in the array
+ *Return: pointer void
  */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int sizetot, i;
-	void *p;
+	char *x;
+	unsigned int a;
 
 	if (nmemb == 0 || size == 0)
-	{
 		return (NULL);
-	}
-	sizetot = nmemb * size;
-	p = malloc(sizetot);
-	if (p == NULL)
-	{
+
+	x = malloc(nmemb * size);
+	if (x == NULL)
 		return (NULL);
-	}
-	for (i = 0; i < sizetot; i++)
-	{
-		*((char *)(p) + i) = 0;
-	}
-	return (p);
+
+	for (a = 0; a < nmemb * size; a++)
+		x[a] = 0;
+
+	return (x);
+
 }
