840cae575c89e85e2228944679398933140c01fe
maxwell150 <maxwelgithumbi@gmail.com>
2021-07-15 20:48:38 +0000

    quick fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,50 @@
-#include "holberton"
+#include <stdlib.h>
+#include <stdio.h>
 /**
-* string_nconcat - concatenates 2 str
-* @s1: destination
-* @s2: source str
-* @n: n.o of bytes to copy
-* Return: concatenated str
-*/
+ *string_nconcat -concatenates two strings upto n
+ *
+ *@s1:string1
+ *@s2:string2
+ *@n:length of string2 to concatenate
+ *Return:pointer
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, size, len1, len2;
-	char *ptr;
+	unsigned int i, len1, len2, length, j;
+	char *p;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	if (n > len2)
+	}
+	len1 = 0;
+	for (i = 0; *(s1 + i) != '\0'; i++)
+		len1++;
+	len2 = 0;
+	for (i = 0; *(s2 + i) != '\0'; i++)
+		len2++;
+	if (n >= len2)
+	{
 		n = len2;
-	size = len1 + n;
-	ptr = malloc(sizeof(char) * size + 1);
-	if (!ptr)
+	}
+	length = len1 + n;
+	p = malloc((sizeof(char) * length) + 1);
+	if (p == NULL)
+	{
 		return (NULL);
+	}
 	for (i = 0; i < len1; i++)
-		ptr[i] = s1[i];
-	for (j = 0; j < n; j++, i++)
-		ptr[i] = s2[j];
-	ptr[i] = 0;
-	return (ptr);
-}
-
-/**
-* _strlen - returns len of str
-* @s: str
-* Return: len of str
-*/
-int _strlen(char *s)
-{
-	char *p = s;
-
-	while (*s)
-		s++;
-	return (s - p);
+	{
+		p[i] = s1[i];
+	}
+	for (j = 0; i < length; j++, i++)
+	{
+		p[i] = s2[j];
+	}
+	p[i] = '\0';
+	return (p);
 }
