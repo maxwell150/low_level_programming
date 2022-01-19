685fe5766f3bd93a95471e4601bca25947425b41
max <maxwelgithumbi@gmail.com>
2022-01-19 17:41:32 +0300

    header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGO_H
+#define SEARCH_ALGO_H
+
+#include <stddef.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
